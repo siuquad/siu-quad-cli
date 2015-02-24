3531b5f6abe122c1b8dfbabb160c6f2b933329f3
Zach Schneider <schneidmaster@gmail.com>
2015-02-24 16:22:35 -0600

    Read img from both drones

--- img.js
+++ img.js
@@ -1,7 +1,12 @@
 var arDrone = require('ar-drone'),
     fs = require('fs')
-var client  = arDrone.createClient();
+var clientOne  = arDrone.createClient({ip: "192.168.1.1"});
+var clientTwo  = arDrone.createClient({ip: "192.168.1.2"});
 
-client.getPngStream().on('data', function(data) {
-  fs.createWriteStream('out.png').write(data)
+clientOne.getPngStream().on('data', function(data) {
+  fs.createWriteStream('droneOne.png').write(data)
+})
+
+clientTwo.getPngStream().on('data', function(data) {
+  fs.createWriteStream('droneTwo.png').write(data)
 })
